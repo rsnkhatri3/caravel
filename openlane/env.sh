a49ce6fe9a99115a1fa14894a0e5a1776546498c
rsnkhatri3 <55553762+rsnkhatri3@users.noreply.github.com>
2020-12-10 14:54:43 +0545

    Add files via upload

---
+++ openlane/env.sh
@@ -0,0 +1,2 @@
+export OPENLANE_ROOT=/home/roshan/Desktop/vsdflow/work/tools/openlane_working_dir/openlane
+export PDK_ROOT=/home/roshan/Desktop/vsdflow/work/tools/openlane_working_dir/pdks/open_pdks/sky130
